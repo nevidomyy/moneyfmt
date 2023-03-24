cf87a36d47af48d0b25c6370d9d38acfb32079c6
Nevidomyy Igor <69916631+nevidomyy@users.noreply.github.com>
2023-03-24 20:45:16 +0300

    Add files via upload

---
+++ moneyfmt.py
@@ -0,0 +1,29 @@
+from decimal import Decimal
+
+def moneyfmt(value, places=2, curr='', sep=' ', dp='.', pos='', neg='-', trailneg=''):
+    q = Decimal(10) ** -places  # 2 places --> '0.01'
+    sign, digits, exp = value.quantize(q).as_tuple()
+    result = []
+    digits = list(map(str, digits))
+    build, next = result.append, digits.pop
+    if sign:
+        build(trailneg)
+    for i in range(places):
+        build(next() if digits else '0')
+    if places:
+        build(dp)
+    if not digits:
+        build('0')
+    i = 0
+    while digits:
+        build(next())
+        i += 1
+        if i == 3 and digits:
+            i = 0
+            build(sep)
+    build(curr)
+    build(neg if sign else pos)
+    return ''.join(reversed(result))
+
+
+
